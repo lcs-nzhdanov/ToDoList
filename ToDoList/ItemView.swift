86625221d3855088c2fdbf620c39938b55671138
Nikita Zhdanov <nzhdanov@lcs.on.ca>
2024-04-05 10:02:51 -0400

    Made the graphical outline. I will use the ternary conditional to tick the boxes, also I will add the model to store the inputed To-Do task. I will go through my older projects to revise how to add something new to the list

---
+++ ToDoList/ItemView.swift
@@ -0,0 +1,18 @@
+//
+//  ItemView.swift
+//  ToDoList
+//
+//  Created by Nikita Zhdanov on 2024-04-05.
+//
+
+import SwiftUI
+
+struct ItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ItemView()
+}
---
+++ ToDoList/LandingView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  ToDoList
+//
+//  Created by Nikita Zhdanov on 2024-04-05.
+//
+
+import SwiftUI
+
+struct LandingView: View {
+    // MARK: Stored properties
+    
+    // The item currently being added
+    @State var newItemDescription: String = ""
+    
+    // The search text
+    @State var searchText = ""
+    
+    // MARK: Computed properties
+    var body: some View {
+        NavigationView {
+            VStack {
+                List {
+                    
+                }
+                    .searchable(text: $searchText)
+                HStack {
+                    
+                }
+                    .padding(20)
+            }
+            .navigationTitle("To do")
+        }
+    }
+}
+
+
+#Preview {
+    LandingView()
+}
