f66e6001d461f90c06c05fd7a4514f53338df3a6
Nikita Zhdanov <nzhdanov@lcs.on.ca>
2024-04-05 09:32:46 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Nikita Zhdanov on 2024-04-05.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
